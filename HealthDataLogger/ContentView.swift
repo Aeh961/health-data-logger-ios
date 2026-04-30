5e74e1c37ce1ed2d6d9cb1babc836c6096f4473d
AEH <117328917+Abdallahelhamawi@users.noreply.github.com>
2026-04-30 05:55:37 -0700

    Initial iOS SwiftUI health data logger

--- HealthDataLogger/ContentView.swift
+++ HealthDataLogger/ContentView.swift
@@ -1,19 +1,39 @@
-//
-//  ContentView.swift
-//  HealthDataLogger
-//
-//  Created by Abdallah El Hamawi on 4/30/26.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var inputText = ""
+    @State private var entries: [String] = []
+    @State private var selectedType = "Health"
+    
+    
     var body: some View {
+        HStack{
+            Button("Health") {
+                selectedType = "Health"
+            }
+            Button ("Activity") {
+                selectedType = "Activity"
+            }
+            
+            Button("Sleep"){
+                selectedType = "sleep"
+            }
+        
+        }
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextField("Enter Health, activity, or sleep data", text:  $inputText).textFieldStyle(.roundedBorder)
+            
+            Button("Add Entry") {
+                if !inputText.isEmpty{
+                    let newEntry = "[\(selectedType)] - (inputText)"
+                    entries.append(newEntry)
+                    inputText = ""
+                }
+            }
+            List(entries, id:\.self) { entry in
+                Text(entry)
+            }
         }
         .padding()
     }
